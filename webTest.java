b67cd768917c4ab64dcb832b728bc3e929d88352
Manoj Sharan <gms.94@icloud.com>
2017-05-16 16:26:34 -0400

    Adding stub file

---
+++ webTest.java
@@ -0,0 +1,37 @@
+package selenium.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+
+public class WebTest
+{
+	private static WebDriver driver;
+	
+	@BeforeClass
+	public static void setUp() throws Exception 
+	{
+		ChromeDriverManager.getInstance().setup();
+		driver = new ChromeDriver();
+	}
+	
+	@AfterClass
+	public static void  tearDown() throws Exception
+	{
+		driver.close();
+		driver.quit();
+	}
+
+
+}
